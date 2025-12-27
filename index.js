b046683a4449cb47bfa0ebd5ac6556eeaed07c92
juandavidguzma09-ai <juandavidguzma09@gmail.com>
2025-12-26 19:12:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, RichPresence } = require('discord.js-selfbot-v13');
 const figlet = require('figlet');
 
 const CONFIG = {
-    TOKEN: ' ',
+    TOKEN: process.env.TOKEN,
     PREFIX: '.',
     BUTTONS: {
         gunslol: 'https://guns.lol/younginfinito',
